7c53ffa9e75a03328b7db3e3ae00a8fc997d2747
iAbhishekPatil <abhishek.patil@joflee.com>
2022-01-08 21:55:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Spotify UI',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        backgroundColor: const Color(0xFF121212),
+        primaryColor: Colors.black,
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: const Color(0xFF1DB954)),
+        iconTheme: const IconThemeData().copyWith(color: Colors.white),
+        fontFamily: 'Montserrat',
+        textTheme: TextTheme(
+          headline2: const TextStyle(
+            color: Colors.white,
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            fontSize: 12.0,
+            color: Colors.grey[300],
+            fontWeight: FontWeight.w500,
+            letterSpacing: 2.0,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.grey[300],
+            fontSize: 14.0,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.grey[300],
+            letterSpacing: 1.0,
+          ),
+        ),
+      ),
+      home: const Shell(),
+    );
+  }
+}
+
+class Shell extends StatelessWidget {
+  const Shell({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Spotify'),
+      ),
+      body: Container(),
+    );
+  }
+}
