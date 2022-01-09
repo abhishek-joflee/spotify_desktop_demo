d13e5f9b5e2a59633eadecca48ad65786c0531f1
iAbhishekPatil <abhishek.patil@joflee.com>
2022-01-09 10:28:27 +0530

    appBar added

--- lib/ui/playList/play_list_screen.dart
+++ lib/ui/playList/play_list_screen.dart
@@ -18,10 +18,86 @@ class _PlayListScreenState extends State<PlayListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('PlayList'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leadingWidth: 140.0,
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              InkWell(
+                customBorder: const CircleBorder(),
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.black26,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Icons.chevron_left,
+                    size: 28.0,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              InkWell(
+                customBorder: const CircleBorder(),
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.black26,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Icons.chevron_right,
+                    size: 28.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.account_circle_outlined, size: 30.0),
+            label: const Text("ABHISHEK"),
+            style: TextButton.styleFrom(
+              primary: Theme.of(context).iconTheme.color,
+            ),
+          ),
+          const SizedBox(width: 8.0),
+          IconButton(
+            splashRadius: 25.0,
+            padding: const EdgeInsets.only(),
+            onPressed: () {},
+            icon: const Icon(
+              Icons.keyboard_arrow_down,
+              size: 30.0,
+            ),
+          ),
+          const SizedBox(width: 20.0),
+        ],
       ),
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              const Color(0xFFAF1018),
+              Theme.of(context).backgroundColor,
+            ],
+            stops: const [0, 0.3],
+          ),
+        ),
+      ),
     );
   }
 }
